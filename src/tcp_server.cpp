bbac8bef66ee9e4316cd3c74a9bcaec0787eee7a
liuhu <liuhu@localhost.localdomain>
2021-12-09 18:28:03 +0800

    封装tcp_server

---
+++ src/tcp_server.cpp
@@ -0,0 +1,110 @@
+/*****************************************************************
+ * Description
+ * Email huxiaoheigame@gmail.com
+ * Created on 2021/12/05
+ * Copyright (c) 2021 虎小黑
+ ****************************************************************/
+
+#include "tcp_server.h"
+
+#include "macro.h"
+
+namespace tigerkin {
+
+static ConfigVar<uint64_t>::ptr g_tcp_server_read_timeout = Config::Lookup("tcpServer.readTimeout", (uint64_t)(60 * 1000 * 2), "tcp server read timeout");
+static ConfigVar<std::string>::ptr g_tcp_server_name = Config::Lookup("tcpServer.name", (std::string)("tigerkin/1.0.0"), "tcp server name/version");
+
+TcpServer::TcpServer(IOManager *workerIOM, IOManager *acceptIOM)
+    : m_workerIOM(workerIOM), m_acceptIOM(acceptIOM), m_rdTimeout(g_tcp_server_read_timeout->getValue()), m_name(g_tcp_server_name->getValue()), m_isStop(true) {
+}
+
+TcpServer::~TcpServer() {
+    for (auto &it : m_sockets) {
+        it->close();
+    }
+    m_sockets.clear();
+}
+
+bool TcpServer::bind(Address::ptr addr) {
+    std::vector<Address::ptr> addrs;
+    std::vector<Address::ptr> errAddrs;
+    addrs.push_back(addr);
+    return bind(addrs, errAddrs);
+}
+
+bool TcpServer::bind(std::vector<Address::ptr> addrs, std::vector<Address::ptr> &errAddrs) {
+    bool rt = true;
+    for (auto &addr : addrs) {
+        Socket::ptr sock = Socket::CreateTCPSocket(addr);
+        if (!sock->bind(addr)) {
+            TIGERKIN_LOG_ERROR(TIGERKIN_LOG_NAME(SYSTEM)) << "TCP SERVER BIND FAIL"
+                                                          << "\n\terrno:" << errno
+                                                          << "\n\terrstr:" << strerror(errno)
+                                                          << "\n\taddr:" << addr->toString();
+            rt = false;
+            errAddrs.push_back(addr);
+            continue;
+        }
+        if (!sock->listen()) {
+            TIGERKIN_LOG_ERROR(TIGERKIN_LOG_NAME(SYSTEM)) << "TCP SERVER LISTEN FAIL"
+                                                          << "\n\terrno:" << errno
+                                                          << "\n\terrstr:" << strerror(errno)
+                                                          << "\n\tsock:" << sock->toString()
+                                                          << "\n\taddr:" << addr->toString();
+            rt = false;
+            errAddrs.push_back(addr);
+            continue;
+        }
+        m_sockets.push_back(sock);
+    }
+    if (rt) {
+        for (auto &sock : m_sockets) {
+            TIGERKIN_LOG_INFO(TIGERKIN_LOG_NAME(SYSTEM)) << "SERVER BIND SUCCESS"
+                                                         << sock->toString();
+        }
+    }
+    return rt;
+}
+
+bool TcpServer::start() {
+    if (!m_isStop) {
+        return true;
+    }
+    m_isStop = false;
+    for (auto &sock : m_sockets) {
+        m_acceptIOM->schedule(std::bind(&TcpServer::startAccept, shared_from_this(), sock));
+    }
+    return true;
+}
+
+void TcpServer::stop() {
+    m_isStop = true;
+    auto self = shared_from_this();
+    m_acceptIOM->schedule([this, self]() {
+        for (auto &sock : m_sockets) {
+            sock->cancelAll();
+            sock->close();
+        }
+        m_sockets.clear();
+    });
+}
+
+void TcpServer::startAccept(Socket::ptr sock) {
+    while (!m_isStop) {
+        Socket::ptr client = sock->accept();
+        if (client) {
+            client->setRecvTimeout(m_rdTimeout);
+            m_workerIOM->schedule(std::bind(&TcpServer::handleClient, shared_from_this(), client));
+        } else {
+            TIGERKIN_LOG_ERROR(TIGERKIN_LOG_NAME(SYSTEM)) << "ACCEPT ERROR:"
+                                                          << "\n\terrno:" << errno
+                                                          << "\n\tstrerror:" << strerror(errno);
+        }
+    }
+}
+
+void TcpServer::handleClient(Socket::ptr client) {
+    TIGERKIN_LOG_INFO(TIGERKIN_LOG_NAME(SYSTEM)) << "handle client";
+}
+
+}  // namespace tigerkin
---
+++ src/tcp_server.h
@@ -0,0 +1,56 @@
+/*****************************************************************
+ * Description
+ * Email huxiaoheigame@gmail.com
+ * Created on 2021/12/05
+ * Copyright (c) 2021 虎小黑
+ ****************************************************************/
+
+#ifndef __TIGERKIN_TCP_SERVER_H__
+#define __TIGERKIN_TCP_SERVER_H__
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "address.h"
+#include "iomamager.h"
+#include "socket.h"
+
+namespace tigerkin {
+
+class TcpServer : public std::enable_shared_from_this<TcpServer> {
+   public:
+    typedef std::shared_ptr<TcpServer> ptr;
+
+    TcpServer(IOManager *workerIOM = IOManager::GetThis(), IOManager *acceptIOM = IOManager::GetThis());
+    ~TcpServer();
+
+    virtual bool bind(Address::ptr addr);
+    virtual bool bind(std::vector<Address::ptr> addrs, std::vector<Address::ptr> &errAddrs);
+    virtual bool start();
+    virtual void stop();
+
+    uint64_t getReadTimeout() const { return m_rdTimeout; }
+    void setReadTimeout(uint64_t v) { m_rdTimeout = v; }
+
+    std::string getName() const { return m_name; }
+    void setName(std::string v) { m_name = v; }
+    bool isStop() const { return m_isStop; }
+
+   protected:
+    virtual void handleClient(Socket::ptr client);
+    virtual void startAccept(Socket::ptr sock);
+
+   private:
+    std::vector<Socket::ptr> m_sockets;
+    IOManager *m_workerIOM;
+    IOManager *m_acceptIOM;
+    uint64_t m_rdTimeout;
+    std::string m_name;
+    bool m_isStop;
+};
+
+}  // namespace tigerkin
+
+#endif  // !_TIGERKIN_TCP_SERVER_H__
