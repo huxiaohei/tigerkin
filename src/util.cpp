048362883b5192e2de8ed49d0261b3c2e9fdbb18
虎小黑 <huxiaoheigame@gmail.com>
2021-12-25 22:09:40 +0800

    丰富工具类 添加StringUtils

--- src/util.cpp
+++ src/util.cpp
@@ -7,6 +7,7 @@
 
 #include "util.h"
 
+#include <stdarg.h>
 #include <execinfo.h>
 #include <time.h>
 
@@ -67,6 +68,181 @@ std::string Time2Str(time_t ts, const std::string &format) {
     char buf[64];
     strftime(buf, sizeof(buf), format.c_str(), &tm);
     return buf;
+}
+
+std::string StringUtils::Format(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    auto v = Formatv(fmt, ap);
+    va_end(ap);
+    return v;
+}
+
+std::string StringUtils::Formatv(const char *fmt, va_list ap) {
+    char *buf = nullptr;
+    int len = vasprintf(&buf, fmt, ap);
+    if (len == -1) {
+        return "";
+    }
+    std::string ret(buf, len);
+    free(buf);
+    return ret;
+}
+
+static const char uriChars[256] = {
+    /* 0 */
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 1, 1, 0,
+    1, 1, 1, 1, 1, 1, 1, 1,   1, 1, 0, 0, 0, 1, 0, 0,
+    /* 64 */
+    0, 1, 1, 1, 1, 1, 1, 1,   1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,   1, 1, 1, 0, 0, 0, 0, 1,
+    0, 1, 1, 1, 1, 1, 1, 1,   1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,   1, 1, 1, 0, 0, 0, 1, 0,
+    /* 128 */
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    /* 192 */
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+static const char xdigitChars[256] = {
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,1,2,3,4,5,6,7,8,9,0,0,0,0,0,0,
+    0,10,11,12,13,14,15,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,10,11,12,13,14,15,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+};
+
+#define CHAR_IS_UNRESERVED(c) (uriChars[(unsigned char)(c)])
+
+std::string StringUtils::UrlEncode(const std::string &str, bool spaceAsPlus) {
+    static const char *hexdigits = "0123456789ABCDEF";
+    std::string *ss = nullptr;
+    const char *end = str.c_str() + str.length();
+    for (const char *c = str.c_str(); c < end; ++c) {
+        if (!CHAR_IS_UNRESERVED(*c)) {
+            if (!ss) {
+                ss = new std::string;
+                ss->reserve(str.size() * 1.2);
+                ss->append(str.c_str(), c - str.c_str());
+            }
+            if (*c == ' ' && spaceAsPlus) {
+                ss->append(1, '+');
+            } else {
+                ss->append(1, '%');
+                ss->append(1, hexdigits[(uint8_t)*c >> 4]);
+                ss->append(1, hexdigits[*c & 0xf]);
+            }
+        } else if (ss) {
+            ss->append(1, *c);
+        }
+    }
+    if (!ss) {
+        return str;
+    } else {
+        std::string rt = *ss;
+        delete ss;
+        return rt;
+    }
+}
+
+std::string StringUtils::UrlDecode(const std::string &str, bool spaceAsPlus) {
+    std::string *ss = nullptr;
+    const char *end = str.c_str() + str.length();
+    for (const char *c = str.c_str(); c < end; ++c) {
+        if (*c == '+' && spaceAsPlus) {
+            if (!ss) {
+                ss = new std::string;
+                ss->append(str.c_str(), c - str.c_str());
+            }
+            ss->append(1, ' ');
+        } else if (*c == '%' && (c + 2) < end && isxdigit(*(c + 1)) && isdigit(*(c + 2))) {
+            if (!ss) {
+                ss = new std::string;
+                ss->append(str.c_str(), c - str.c_str());
+            }
+            ss->append(1, (char)(xdigitChars[(int)*(c + 1)] << 4 | xdigitChars[(int)*(c+ 2)]));
+            c += 2;
+        } else if (ss) {
+            ss->append(1, *c);
+        }
+    }
+    if (!ss) {
+        return str;
+    } else {
+        std::string rt = *ss;
+        delete ss;
+        return rt;
+    }
+}
+
+std::string StringUtils::Trim(const std::string &str, const std::string &delimit) {
+    auto begin = str.find_first_not_of(delimit);
+    if (begin == std::string::npos) {
+        return "";
+    }
+    auto end = str.find_last_not_of(delimit);
+    return str.substr(begin, end - begin + 1);
+}
+
+std::string StringUtils::TrimLeft(const std::string &str, const std::string &delimit) {
+    auto begin = str.find_first_not_of(delimit);
+    if (begin == std::string::npos) {
+        return "";
+    }
+    return str.substr(begin);
+}
+
+std::string StringUtils::TrimRight(const std::string &str, const std::string &delimit) {
+    auto end = str.find_last_not_of(delimit);
+    if (end == std::string::npos) {
+        return "";
+    }
+    return str.substr(0, end);
+}
+
+std::string StringUtils::WStringToString(const std::wstring &w) {
+    std::string strLocale = setlocale(LC_ALL, "");
+    const wchar_t *wchSrc = w.c_str();
+    size_t nDestSize = wcstombs(NULL, wchSrc, 0) + 1;
+    char *chDest = new char[nDestSize];
+    memset(chDest, 0, nDestSize);
+    wcstombs(chDest, wchSrc, nDestSize);
+    std::string strResult = chDest;
+    delete []chDest;
+    setlocale(LC_ALL, strLocale.c_str());
+    return strResult;
+}
+
+std::wstring StringUtils::StringToWString(const std::string &s) {
+    std::string strLocle = setlocale(LC_ALL, "");
+    const char *chSrc = s.c_str();
+    size_t nDestSize = mbstowcs(NULL, chSrc, 0) + 1;
+    wchar_t *wchDest = new wchar_t[nDestSize];
+    wmemset(wchDest, 0, nDestSize);
+    mbstowcs(wchDest, chSrc, nDestSize);
+    std::wstring wstrResult = wchDest;
+    delete []wchDest;
+    setlocale(LC_ALL, strLocle.c_str());
+    return wstrResult;
 }
 
 }  // namespace tigerkin
--- src/util.h
+++ src/util.h
@@ -1,5 +1,5 @@
 /*****************************************************************
- * Description 
+ * Description
  * Email huxiaoheigame@gmail.com
  * Created on 2021/08/01
  * Copyright (c) 2021 虎小黑
@@ -28,7 +28,26 @@ std::string BacktraceToString(int size = 100, int skip = 2, const std::string &p
 std::time_t GetNowMillisecond();
 std::time_t GetNowSecond();
 
-std::string Time2Str(time_t ts = time(0), const std::string& format = "%Y-%m-%d %H:%M:%S");
+std::string Time2Str(time_t ts = time(0), const std::string &format = "%Y-%m-%d %H:%M:%S");
+
+class StringUtils {
+   
+   public:
+    
+    static std::string Format(const char *fmt, ...);
+    static std::string Formatv(const char *fmt, va_list ap);
+
+    static std::string UrlEncode(const std::string &str, bool spaceAsPlus = true);
+    static std::string UrlDecode(const std::string &str, bool spaceAsPlus = true);
+
+    static std::string Trim(const std::string &str, const std::string &delimit = " \t\r\n");
+    static std::string TrimLeft(const std::string &str, const std::string &delimit = " \t\r\n");
+    static std::string TrimRight(const std::string &str, const std::string &delimit = " \t\r\n");
+
+    static std::string WStringToString(const std::wstring &w);
+    static std::wstring StringToWString(const std::string &s);
+
+};
 
 }  // namespace tigerkin
 
