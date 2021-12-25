6b1689d18aeb412dd02d93f6952840e39106e0ca
虎小黑 <huxiaoheigame@gmail.com>
2021-12-25 22:13:32 +0800

    丰富单元测试

--- tests/http_server_test.cpp
+++ tests/http_server_test.cpp
@@ -8,7 +8,7 @@
 #include "../src/tigerkin.h"
 
 void run() {
-    tigerkin::http::HttpServer::ptr server(new tigerkin::http::HttpServer());
+    tigerkin::http::HttpServer::ptr server(new tigerkin::http::HttpServer);
     tigerkin::Address::ptr addr = tigerkin::IpAddress::LookupAnyIpAddress("0.0.0.0:8080");
     while (!server->bind(addr)) {
         sleep(2);
@@ -17,7 +17,39 @@ void run() {
     dsp->addServlet("/hello", [](tigerkin::http::HttpRequest::ptr req,
                                  tigerkin::http::HttpResponse::ptr rsp,
                                  tigerkin::http::HttpSession::ptr sess) {
+        TIGERKIN_LOG_DEBUG(TIGERKIN_LOG_NAME(TEST)) << "params:";
+        const auto params = req->getParams();
+        for (auto &it : params) {
+            TIGERKIN_LOG_DEBUG(TIGERKIN_LOG_NAME(TEST)) << "\t" << it.first << ":" << it.second;
+        }
+
+        TIGERKIN_LOG_DEBUG(TIGERKIN_LOG_NAME(TEST)) << "cookies:";
+        const auto cookies = req->getCookies();
+        for (auto &it : cookies) {
+            TIGERKIN_LOG_DEBUG(TIGERKIN_LOG_NAME(TEST)) << "\t" << it.first << ":" << it.second;
+        }
+
+        TIGERKIN_LOG_DEBUG(TIGERKIN_LOG_NAME(TEST)) << "headers:";
+        const auto headers = req->getHeaders();
+        for (auto &it : headers) {
+            TIGERKIN_LOG_DEBUG(TIGERKIN_LOG_NAME(TEST)) << "\t" << it.first << ":" << it.second;
+        }
+
         rsp->setBody("Hello I'm tigerkin!");
+        rsp->setHeader("result", "ok");
+        rsp->setCookies("cookie", "tigerkin");
+
+        return 0;
+    });
+
+    dsp->addServlet("/close", [server](tigerkin::http::HttpRequest::ptr req,
+                                       tigerkin::http::HttpResponse::ptr rsp,
+                                       tigerkin::http::HttpSession::ptr sess) {
+        rsp->setBody("Ok");
+        server->stop();
+        tigerkin::IOManager::GetThis()->schedule([]() {
+            tigerkin::IOManager::GetThis()->stop();
+        }, tigerkin::IOManager::GetThis()->getCallerThreadId());
         return 0;
     });
     server->start();
